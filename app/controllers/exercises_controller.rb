14e6c403e72abeda0334cf93e387601181566a2e
Exellin <carnegieshawn@yahoo.ca>
2016-11-09 17:24:08 +0000

    add show and create actions and show view

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,8 +6,23 @@ class ExercisesController < ApplicationController
     @exercise = current_user.exercises.new
   end
   
+  def show
+    @exercise = current_user.exercises.find params[:id]
+  end
+  
   def create
+    @exercise = current_user.exercises.new(exercise_params)
+    if @exercise.save
+      flash[:notice] = "Exercise has been created"
+      redirect_to user_exercise_path(current_user, @exercise)
+    else
+      flash[:alert] = "Exercise has not been created"
+      render :new
+    end
+  end
+  
+  private
+  def exercise_params
+    params.require(:exercise).permit(:duration_in_min, :workout, :workout_datem, :user_id)
   end
 end
-  
-  
