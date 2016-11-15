30eb56b49a709b4171456b06ce881d501bda11dd
Exellin <carnegieshawn@yahoo.ca>
2016-11-15 06:00:02 +0000

    implement delete function

--- spec/features/exercises/deleting_exercise_spec.rb
+++ spec/features/exercises/deleting_exercise_spec.rb
@@ -16,11 +16,11 @@ RSpec.feature "Deleting Exercise" do
     click_link "My Lounge"
     
     path = "/users/#{@owner.id}/exercises/#{@owner_exercise.id}"
-    link = "//a[contains(@href,\'#{path\}') and .//text()='Destroy']"
+    link = "//a[contains(@href,\'#{path}\') and .//text()='Destroy']"
     
-    find(:xpath, link).click_link
+    find(:xpath, link).click
     
-    expect(path).to have_content("Exercise has been deleted")
+    expect(page).to have_content("Exercise has been deleted")
   end
     
 end
