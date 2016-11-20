60f123aceb58eafa4a124ed25aebfec4b1377de4
Exellin <carnegieshawn@yahoo.ca>
2016-11-20 05:48:08 +0000

    add messages

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -3,6 +3,10 @@ class ExercisesController < ApplicationController
   def index
     @exercises = current_user.exercises
     @friends = current_user.friends
+    set_current_room
+    @message = Message.new
+    @messages = current_room.messages if current_room
+    @followers = Friendship.where(friend_id: current_user.id)
   end
   
   def new
@@ -42,12 +46,23 @@ class ExercisesController < ApplicationController
     redirect_to user_exercises_path(current_user)
   end
   
+  
+  private
+  
   def set_exercise
     @exercise = current_user.exercises.find params[:id]
   end
   
-  private
   def exercise_params
     params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+  end
+  
+  def set_current_room
+    if params[:room_id]
+      @room = Room.find_by(id: params[:room_id])
+    else
+      @room=current_user.room
+    end
+    session[:current_room] = @room.id if @room
   end
 end
